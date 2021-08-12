75d48fe1ca50e05a0455503ae5df5845e457c5ea
Douglas Berkley <douglasmberkley@gmail.com>
2021-08-12 15:15:35 +0900

    added planet seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,52 @@ Alien.destroy_all if Rails.env.development?
 
 alien = Alien.new(name: 'Zorg', banner_url: 'https://i.imgur.com/DZdFxS6.jpeg')
 alien.save!
+
+Planet.create!(
+  name: "Earth",
+  image_url: "https://cdn.mos.cms.futurecdn.net/3upZx2gxxLpW7MBbnKYQLH-1200-80.jpg",
+  type: "solid",
+  alien: alien
+)
+
+Planet.create!(
+  name: "Mars",
+  image_url: "https://space-facts.com/wp-content/uploads/mars-v2.jpg",
+  type: "solid",
+  alien: alien
+)
+
+Planet.create!(
+  name: "Tatooine",
+  image_url: "https://static.wikia.nocookie.net/starwars/images/b/b0/Tatooine_TPM.png/revision/latest?cb=20150814063047&path-prefix=ja",
+  type: "solid",
+  alien: alien
+)
+
+Planet.create!(
+  name: "Hoth",
+  image_url: "https://static.wikia.nocookie.net/starwars/images/8/81/Hoth_AoRCR.png/revision/latest?cb=20170222025915",
+  type: "solid",
+  alien: alien
+)
+
+Planet.create!(
+  name: "Coruscant",
+  image_url: "https://static.wikia.nocookie.net/starwars/images/1/16/Coruscant-EotE.jpg/revision/latest?cb=20130908205521",
+  type: "solid",
+  alien: alien
+)
+
+Planet.create!(
+  name: "Naboo",
+  image_url: "https://static.wikia.nocookie.net/starwars/images/4/41/Naboo_FFGRebellion.png/revision/latest?cb=20170529051916",
+  type: "solid",
+  alien: alien
+)
+
+Planet.create!(
+  name: "Vulcan",
+  image_url: "https://wp-assets.futurism.com/2013/02/200635_414242988662116_1691606020_n.jpg",
+  type: "solid",
+  alien: alien
+)
